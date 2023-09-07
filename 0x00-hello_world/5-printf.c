9d8cbe5aaef82e9b1d2017a06ccc4172f30fba38
briviamoon <briviamoon@gmail.com>
2023-09-07 09:18:11 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - beginning of the execution
+ *Return: 0 if seccessful
+ */
+
+int main(void)
+{
+	pritf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
