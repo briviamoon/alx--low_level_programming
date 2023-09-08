853100491f4856abf27f87cf514b03f0660fda65
briviamoon <briviamoon@gmail.com>
2023-09-08 12:04:56 -0700

    last digit using modular

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point.
+ *Return: Always 0 if success.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int last_num = (n % 10);
+
+	if (last_num > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, last_num);
+	}
+	else if (last_num == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, last_num);
+	}
+	else if (last_num < 6 && != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_num);
+	}
+	return (0);
+}
