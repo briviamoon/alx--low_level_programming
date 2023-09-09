181c14ab485a7919456fe12332ed5b35393288d8
briviamoon <briviamoon@gmail.com>
2023-09-09 14:42:50 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Starting point.
+ *Return: 0 when succesfull.
+ */
+
+int main(void)
+{
+	int a = '0';
+
+	while (a <= 9)
+	{
+		putchar(a + '0');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
