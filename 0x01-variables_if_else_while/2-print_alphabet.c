0f76bf1f5403f3586d921a3efdd19400a1066371
briviamoon <briviamoon@gmail.com>
2023-09-09 13:15:52 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Start of process.
+ *Return: 0 if successful.
+ */
+
+int main(void)
+{
+	for (char letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	return (0);
+}
