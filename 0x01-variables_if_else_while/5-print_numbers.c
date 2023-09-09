332793695c9387bd29f1d12b5d55cdd5871ffdc3
briviamoon <briviamoon@gmail.com>
2023-09-09 14:35:33 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Starting point.
+ *Return: 0 when successful.
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a)
+		a++;
+	}
+	return (0);
+}
