c8aa612cb6de4f13be61c7fb65c7583b27d472fa
briviamoon <briviamoon@gmail.com>
2023-09-09 15:03:22 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,12 @@
 
 int main(void)
 {
-	char case = 'z';
+	char c = 'z';
 
-	while (case >= 'a')
+	while (c >= 'a')
 	{
-		putchar(case);
-		case--;
+		putchar(c);
+		c--;
 	}
 	putchar('\n');
 	return (0);
