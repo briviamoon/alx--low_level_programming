89ab91c4ce8a2f2f1f8bb296352d08aef21fc95c
briviamoon <briviamoon@gmail.com>
2023-09-12 11:24:49 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_last_digit - Prints last digit of number.
+ *@arg: the type from input (int).
+ *Return: last> the value of last digit,
+ *	  0 if digit is zero or none numeric type.
+ */
+
+int print_last_digit(int arg)
+{
+	if (arg < 0 && arg > 0)
+	{
+		last = arg % 10;
+
+		return (last);
+	}
+	else if (args == 0)
+	{
+		return (args);
+	}
+	else
+	{
+		return (0);
+	}
+}
