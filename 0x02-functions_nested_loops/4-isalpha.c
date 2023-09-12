ab165c9d977c5b87a7cbb36cb343660a7a7cdf62
briviamoon <briviamoon@gmail.com>
2023-09-12 09:43:04 -0700

    _isalpha-3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *_isalpha - Checks for alphabets.
+  *_isalpha - Checks for alphabets.
  *@c: the char to be checked.
  */
 
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	else
+	else if (c >= 'A' && c <= 'Z')
 		return (0);
 }
