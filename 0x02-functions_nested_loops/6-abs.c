b11f13cc1792dfa8db8981c3d816c6fd9e073dcf
briviamoon <briviamoon@gmail.com>
2023-09-12 10:46:47 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_abs - prints the absolute value of a number.
+ *Return: val when successful.
+ */
+
+int _abs(int)
+{
+	if (int < 0 )
+	{
+		int val = int * -1;
+		return (val);
+	}
+	else if (int > 0)
+	{
+		int val = int * 1;
+		return (val);
+	}
+	else
+	{
+		return (val);
+	}
+}
