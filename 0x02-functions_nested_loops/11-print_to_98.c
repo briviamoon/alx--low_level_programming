b43d5e41f86fc22fc61287f4268e1601a4bd3dc4
briviamoon <briviamoon@gmail.com>
2023-09-12 16:58:08 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_to_98 - prints from input number till 98.
+ *@n: the input numer.
+ */
+
+void print_to_98(int n)
+{
+	for (int n; n <= 98; n++)
+	{
+		printf("%d, ", n);
+	}
+}
