a8ad9adc4998b61500849020cd7d03b518ec0e19
briviamoon <briviamoon@gmail.com>
2023-09-12 08:51:55 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_islower - informs lower case characters.
+ *Return: is_it value.
+ */
+
+int _islower(int c)
+{
+	char low_case = 'a';
+	int is_it = 0;
+
+	while (low_case <= 'z')
+	{
+		if (low_case == c)
+		{
+			is_it = 0;
+		}
+		low_case++;
+	}
+	return (is_it);
+}
