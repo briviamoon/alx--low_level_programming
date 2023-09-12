9eed5889f82f7a56007810a2cd6ac4df3892eae7
briviamoon <briviamoon@gmail.com>
2023-09-12 15:53:33 -0700

    table of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,24 @@ void times_table(void)
 {
 	int first;
 	int sec;
+	int result;
 
 	for (first = 0; first <= 9; first++)
 	{
+		_putchar(0);
+
 		for (sec = 0; sec <= 9; sec++)
 		{
-			_putchar((first * sec) + '0');
 			_putchar(',');
 			_putchar(' ');
+
+			result = first * sec;
+
+			if (result <= 9)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
 	}
