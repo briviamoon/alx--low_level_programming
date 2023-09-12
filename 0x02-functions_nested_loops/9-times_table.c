18b4694d68fda1c05e204a9d970666181be2e82d
briviamoon <briviamoon@gmail.com>
2023-09-12 15:20:42 -0700

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *times_table - multiplies 9 in 2 dimentions.
+ *
+ */
+
+void times_table(void)
+{
+	int first;
+	int sec;
+
+	for (first = 0; first <= 9; first++)
+	{
+		for (sec = 0; sec <= 9; sec++)
+		{
+			_putchar((first * sec) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
