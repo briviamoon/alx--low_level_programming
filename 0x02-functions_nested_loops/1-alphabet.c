374b89850c837aa3acfb615ce49272bb1d192643
briviamoon <briviamoon@gmail.com>
2023-09-12 07:53:18 -0700

    1-print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints alphabets.
+ *Return: 0 when success.
+ */
+
+void print_alphabet(void)
+{
+	char small_letter = 'a';
+
+	while (small_letter <= 'z')
+	{
+		_putchar(small_letter);
+		small_letter++;
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define FUNCTIONS_H
 #include<stdio.h>
 int _putchar(char c);
+void print_alphabet(char c);
 #endif
