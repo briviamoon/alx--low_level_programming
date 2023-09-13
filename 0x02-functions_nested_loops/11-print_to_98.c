af6a7690d4407548244952e20531e251c70bb62d
briviamoon <briviamoon@gmail.com>
2023-09-12 17:13:07 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,9 +6,21 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n == 98)
 	{
-		printf("%d, ", n);
-		n++;
+		printf("%d", n);
+	}
+	else if
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
 	}
 }
