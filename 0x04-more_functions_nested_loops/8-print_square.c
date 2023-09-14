1d4e636e24657e1786af625a62a583912ada1c12
briviamoon <briviamoon@gmail.com>
2023-09-14 08:29:55 -0700

    spongebob

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_square - prints a square pattern.
+ *@size: dimensions of square.
+ */
+
+void print_square(int size)
+{
+	char block = '#';
+	int blockNo;
+	int count;
+
+	if (size > 0)
+	{
+		for (count = 0; count < size; count++)
+		{
+			for (blockNo = 0; blockNo < size; blockNo++)
+			{
+				_putchar(block);
+			}
+			_putchar('\n');
+		}
+	}
+}
