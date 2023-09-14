4abf0a01e7fdb76fd7e8528aa93715728cf50e76
briviamoon <briviamoon@gmail.com>
2023-09-14 09:32:10 -0700

    coding my way out of a wet paper bag

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,20 +12,20 @@ void fizz_buzz(void)
 
 		if (count % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (count % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (count % 3 == 0 && count % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%d", count);
+			printf("%d ", count);
 		}
-		printf(" ");
+		printf('\n');
 	}
 }
