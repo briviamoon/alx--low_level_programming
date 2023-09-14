8dfc4d63d5e6a574279743a9924117bcc86e07b5
briviamoon <briviamoon@gmail.com>
2023-09-14 07:04:06 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_line - prints a line.
+ *@n: argument stating number of lines.
+ */
+
+void print_line(int n)
+{
+	int lineNumb = 0;
+	char line = '_';
+
+	if (n > lineNumb)
+	{
+		for (lineNumb = 0; lineNum < n; lineNum++)
+		{
+			_putchar(line);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
