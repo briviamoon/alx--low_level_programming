cff04f62751180fb37e488dc05877c8ca798745c
briviamoon <briviamoon@gmail.com>
2023-09-20 23:49:48 +0300

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words if a string.
+ * @str: string to capitalize.
+ * Return: str.
+*/
+
+char *cap_string(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z' )
+		{
+			*ptr = *ptr + ('A' - 'a');
+		}
+		else if
+		ptr++;
+	}
+	return (str);
+}
