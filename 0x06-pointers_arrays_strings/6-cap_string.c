2d21dbfafd52849019c7696a3417ab5ab1cdd144
briviamoon <briviamoon@gmail.com>
2023-09-21 13:23:59 +0300

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words if a string.
+ * cap_string - capitalizes first letter of every word in a string.
+ *				starting from the first word, and every word with,
+ *				a separator before it.
  * @str: string to capitalize.
  * Return: str.
  */
@@ -9,27 +11,52 @@
 char *cap_string(char *str)
 {
 	char *ptr = str;
-	char separator[] = ",;.!?\"(){}";
-	int counter;
-	int marker;
+	int marker = 1;
 
 	while (*ptr != '\0')
 	{
-		for (counter = 0; separator[counter] != '\0'; counter++)
+		/*chaeck if small letter*/
+		if (*ptr >= 'a' && *ptr <= 'z')
 		{
-			if (*ptr == separator[counter])
+			/*marker to look to signal capitalization*/
+			if (marker)
 			{
-				marker = 1;
-				break;
+				/*check if current leter is current letter of word*/
+				/*check if it follows a separator && not capitalized*/
+				if (ptr == str || separator(*(ptr - 1)))
+				{
+					/*capitalize by Ascii arithmetics*/
+					*ptr = *ptr + ('A' - 'a');
+				}
 			}
-		}
-
-		if (marker && *ptr >= 'a' && *ptr <= 'z')
-		{
-			*ptr = *ptr + ('A' - 'a');
 			marker = 0;
+		}
+		else if (separator(*ptr))
+		{
+			marker = 1;
 		}
 		ptr++;
 	}
 	return (str);
+}
+
+/**
+ * separator - check if char is a separator.
+ * @s: argument for character to check.
+ * Return: 1 when char is reparator.
+ *			0 when isn't.
+*/
+int separator(char s)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int counter;
+
+	for (counter = 0; separators[counter] != '\0'; counter++)
+	{
+		if (s == separators[counter])
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
