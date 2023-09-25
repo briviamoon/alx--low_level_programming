01d2a0926208612e892ccdf8fc680915a3c571a5
briviamoon <briviamoon@gmail.com>
2023-09-25 12:37:44 +0300

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,14 @@
  * @s: memory area to fill.
  * @b: constant byte.
  * @n: number of bytes to be filled.
+ * Return: final s value 
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *p = s;
 	unsigned int index;
+
 	for (index = 0; index < n; index++)
 	{
 		*p = b;
