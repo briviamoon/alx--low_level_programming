98d512aeaa2c520faab0ba0bd616975c4ff13de8
briviamoon <briviamoon@gmail.com>
2023-09-29 14:35:10 +0300

    what's my name?

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,16 @@
 /**
  * main - prints its name
  * @argv: aray of  strings in cmd line.
- * @argc: unused number of cmd.
+ * @argc: number of cmd args
  * Return: 0 if success
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
-
-	printf("%s", argv[i]);
-	putchar('\n');
+	if (argc == 1 && argv[0] != NULL)
+	{
+		printf("%s", argv[0]);
+		putchar('\n');
+	}
 	return (0);
 }
