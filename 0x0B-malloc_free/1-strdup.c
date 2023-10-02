fd456459dd2d6450f4e5289518ad3b8e479db68f
briviamoon <briviamoon@gmail.com>
2023-10-02 10:06:12 +0300

    indentation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,26 +4,26 @@
  * _strdup - returns pointer to string duplicate
  * @str: pointer to string
  * Return: pointer to copy. NULL if str = NUL.
-*/
+ */
 
 char *_strdup(char *str)
 {
-    unsigned int i;
-    char *ch;
+	unsigned int i;
+	char *ch;
 
-    ch = (char *)malloc(sizeof(char) * sizeof(str));
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    if (ch == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < sizeof(str); i++)
-    {
-        ch[i] = str[i];
-    }
-    ch[sizeof(str)] = '\0';
-    return (ch);
+	ch = (char *)malloc(sizeof(char) * sizeof(str));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (ch == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < sizeof(str); i++)
+	{
+		ch[i] = str[i];
+	}
+	ch[sizeof(str)] = '\0';
+	return (ch);
 }
