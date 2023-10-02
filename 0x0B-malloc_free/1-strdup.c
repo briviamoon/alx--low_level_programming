7d4d547c11ca767c2fcc885bbd3896840ae85538
briviamoon <briviamoon@gmail.com>
2023-10-02 09:53:39 +0300

    duplicate str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strdup - returns pointer to string duplicate
+ * @str: pointer to string
+ * Return: pointer to copy. NULL if str = NUL.
+*/
+
+char *_strdup(char *str)
+{
+    unsigned int i;
+    char *ch;
+
+    ch = (char *)malloc(sizeof(str));
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < sizeof(str); i++)
+    {
+        ch = str;
+    }
+    return (ch);
+}
