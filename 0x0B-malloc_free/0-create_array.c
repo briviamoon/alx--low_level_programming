2b48cf43aef9b39bd70224f83900f8c99c451979
briviamoon <briviamoon@gmail.com>
2023-10-02 09:35:01 +0300

    fix indent

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,24 +9,24 @@
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *ch;
-    
-    ch = (char *)malloc(sizeof(char) * size);
-    if (size == 0)
-    {
-        return (NULL);
-    }
-    
-    if (ch == NULL)
-    {
-        return (NULL);
-    }
-    
-    for (i = 0; i < size; i++)
-    {
-        ch[i] = c;
-    }
-    
-    return (ch);
+	unsigned int i;
+	char *ch;
+
+	ch = (char *)malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (ch == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		ch[i] = c;
+	}
+
+	return (ch);
 }
