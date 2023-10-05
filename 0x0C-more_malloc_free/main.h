22d01ce048820021bc30890c9571c715af3c9c12
briviamoon <briviamoon@gmail.com>
2023-10-05 14:10:37 +0300

    2-calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 int multipy(int mul, int num1, int num2);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
