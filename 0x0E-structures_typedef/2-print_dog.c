aca8d4e401cf782b2a32bde63045e0dc97249e84
briviamoon <briviamoon@gmail.com>
2023-10-09 13:02:21 +0300

    printing dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,13 +6,11 @@
 */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		printf("Name: %s", d->name);
-		printf("\n");
-		printf("Age: %6f", d->age);
-		printf("\n");
-		printf("Owner: %s", d->owner);
-		printf("\n");
-	}
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
