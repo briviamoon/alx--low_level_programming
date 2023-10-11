0cf182e55faea8b11b8a2ead2e48a844682e4722
briviamoon <briviamoon@gmail.com>
2023-10-11 18:56:12 +0300

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,6 +24,11 @@ int main(int argc, char *argv[])
 	sign = argv[2];
 	num2 = atoi(argv[3]);
 
+	if (((*sign == '/') && (num2 == 0)) || ((*sign == '%') && (num2 == 0)))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	fun = get_op_func(sign);
 
 	if (fun == NULL)
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -54,11 +54,6 @@ int op_div(int a, int b)
 {
 	int result;
 
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	result = a / b;
 
 	return (result);
@@ -74,11 +69,6 @@ int op_mod(int a, int b)
 {
 	int result;
 
-	if (b == 0)
-	{
-		printf("Error");
-		exit(100);
-	}
 	result = a % b;
 	return (result);
 }
