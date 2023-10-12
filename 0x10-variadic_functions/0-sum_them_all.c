9a6c25cb3192afe54c49798f5e957f82146a4f0e
briviamoon <briviamoon@gmail.com>
2023-10-12 12:27:39 +0300

    summ

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all - prints sum of all parameters
+ *@n: first argument
+ *Return: 0 if n == 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum;
+	va_list ptr;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	return (sum);
+}
+
