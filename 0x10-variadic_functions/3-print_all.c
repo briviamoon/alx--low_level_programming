46266c846fcbb8f86630320dbf454b076c4729ee
briviamoon <briviamoon@gmail.com>
2023-10-12 15:59:49 +0300

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ *print_all - prints anything
+ *@format: list of argument types
+ */
+void print_all(const char *const format, ...)
+{
+	va_list ptr;
+	char *separator = "";
+	int i = 0;
+	char *str;
+
+	va_start(ptr, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%s%c", separator, va_arg(ptr, int));
+			break;
+		case 'i':
+			printf("%s%d", separator, va_arg(ptr, int));
+			break;
+		case 'f':
+			printf("%s%f", separator, va_arg(ptr, double));
+			break;
+		case 's':
+			str = va_arg(ptr, char *);
+			if (str == NULL)
+			{
+				printf("(nil)");
+			}
+			if (str != NULL)
+			{
+				printf("%s%s", separator, str);
+			}
+			break;
+		default:
+			i++;
+			continue;
+		}
+		separator = ", ";
+		i++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
