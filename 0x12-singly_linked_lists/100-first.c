28dfb96f4ea894c664129ab589f2037ba9588a8f
briviamoon <briviamoon@gmail.com>
2023-10-20 01:35:12 +0300

    pre Main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 
+/**
+ * preMain - prints item before main is executed.
+*/
 void preMain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
