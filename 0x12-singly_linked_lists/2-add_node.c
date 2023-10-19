50cfa4f416ecbfb368f048d3b6767d034e184158
briviamoon <briviamoon@gmail.com>
2023-10-19 22:41:56 +0300

    2-add_node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ *add_node - adds new node to begining of list
+ *@head: pointer to new node
+ *@str: pointer to string member
+ *Return: address of new element or NULL if failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(new_node->str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
