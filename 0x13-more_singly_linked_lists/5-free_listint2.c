0d0ad17676827fbceaf2588593a090ae53037260
briviamoon <briviamoon@gmail.com>
2023-10-24 00:28:36 +0300

    5. free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,9 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *next;
+	listint_t *current, *temporary;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 	{
 		return;
 	}
@@ -17,9 +17,9 @@ void free_listint2(listint_t **head)
 
 	while (current != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		temporary = current;
+		current = current->next;
+		free(temporary);
 	}
 	*head = NULL;
 }
