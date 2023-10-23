d913456ca3346b469743acf1bb29bfe995d08a53
briviamoon <briviamoon@gmail.com>
2023-10-23 23:38:21 +0300

    8 and 9

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - finds sum of all data in (n) element
+ * @head: pointer to node
+ * Return: sum of all data
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head ==NULL)
+	{
+		return (0);
+	}
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to a pointer to a node
+ * @idx: index where to add.
+ * @n: element in new node
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode, *current;
+	unsigned int nth = 0;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->n = n;
+	if (*head == NULL)
+	{
+		*head = newNode;
+	}
+	current = *head;
+	while (current->next != NULL)
+	{
+		if (nth == idx)
+		{
+			current->next = newNode;
+			newNode->next = current;
+		}
+		current = current->next;
+	}
+	return (newNode);
+	free(newNode);
+}
