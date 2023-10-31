21b560ae082024f790eb9c3d99383dd437ac3fa3
briviamoon <briviamoon@gmail.com>
2023-10-31 11:58:29 +0300

    create file and write to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fileDescriptor = open(filename, O_CREAT | O_WRONLY, O_TRUNC, 0600);
-	close(fileDescriptor);
-
 	fileDescriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fileDescriptor == -1)
 	{
