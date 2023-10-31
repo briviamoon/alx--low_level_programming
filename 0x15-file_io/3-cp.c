952f8d5b0e14b8e93b6c09740ac3fd9362282cd1
briviamoon <briviamoon@gmail.com>
2023-10-31 14:43:56 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,65 +1,75 @@
 #include "main.h"
 
 /**
- * error - formats and prints error message in POSIX std.
+ * error - handle error
  * @code: exit code.
- * @format: string error message.
- */
-void error(int code, const char *format, ...)
+ * @format: error message
+ * @arg: arg related to error
+*/
+void error(int code, const char *format, const char *arg)
 {
-	va_list args;
-
-	va_start(args, format);
-	dprintf(STDERR_FILENO, format, args);
-	va_end(args);
+	dprintf(STDERR_FILENO, format, arg);
 	exit(code);
 }
 
 /**
- * main - copies content of one file to other.
- * @argc: number of arguments
- * @argv: string of argument strings.
- * Return: 0 if siccess.
- */
+ * errorFD - hadle for FD.
+ * @code: exit code.
+ * @format: string to print to stdout
+ * @FD: file descriptor value related to error
+*/
+void errorFD(int code, const char *format, int FD)
+{
+	dprintf(STDERR_FILENO, format, FD);
+	exit(code);
+}
+
+
+/**
+ * main - copy from file1 to file2
+ * @argc: number of args
+ * @argv: arg  string
+ * Return: 0 if success.
+*/
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	char buffer[1024];
+	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		error(97, "Usage: cp file_from file_to\n");
-
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
 	{
-		close(file_from);
+		error(97, "Usage: %s file_from file_to\n", argv[0]);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
 		error(98, "Error: Can't read from file %s\n", argv[1]);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		close(file_to);
 		error(99, "Error: Can't write to file %s\n", argv[2]);
 	}
-	bytes_read = read(file_from, buffer, sizeof(buffer));
-	while (bytes_read > 0)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(file_to, buffer, bytes_read);
+		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
-			close(file_from);
-		close(file_to);
-		error(99, "Error: Can't write to file %s\n", argv[2]);
+		{
+			error(99, "Error: Can't write to file %s\n", argv[2]);
+		}
 	}
-
 	if (bytes_read == -1)
+	{
 		error(98, "Error: Can't read from file %s\n", argv[1]);
-
-	if (close(file_from) == -1)
-		error(100, "Error: Can't close fd %d\n", file_from);
-
-	if (close(file_to) == -1)
-		error(100, "Error: Can't close fd %d\n", file_to);
-
+	}
+	if (close(fd_from) == -1)
+	{
+		errorFD(100, "Error: Can't close fd %d\n", fd_from);
+	}
+	if (close(fd_to) == -1)
+	{
+		errorFD(100, "Error: Can't close fd %d\n", fd_to);
+	}
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,13 +11,14 @@
 #include <stdarg.h>
 #include <errno.h>
 #include <elf.h>
-#define _GNU_SOURCE
 
 #define BUFFER_SIZE 1024
+#define _POSIX_C_SOURCE 200809L
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error(int code, const char *format, ...);
+void error(int code, const char *format, const char *arg);
+void errorFD(int code, const char *format, int FD);
 
 #endif
