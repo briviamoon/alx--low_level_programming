efa0b9e0b474b7f4082b55d17e997864cfcb609e
briviamoon <briviamoon@gmail.com>
2023-10-31 11:52:03 +0300

    create - write a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		fileDescriptor = open(filename, O_CREAT);
+		fileDescriptor = open(filename, O_CREAT | O_WRONLY, O_TRUNC, 0600);
 		close(fileDescriptor);
 	}
 	fileDescriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
